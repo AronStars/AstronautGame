235129d680cecf5122246bbf66029112a6d118a0
GINGERWASTAKEN <oscarbaetu007@gmail.com>
2024-05-12 11:20:56 +0100

    Added camerra movement

--- Pygame Basic Movement Bitmap Collision with Gravity.py
+++ Pygame Basic Movement Bitmap Collision with Gravity.py
@@ -4,11 +4,12 @@ pygame.init()
 scrn = pygame.display.set_mode((600, 400))
 clock = pygame.time.Clock()
 
-#player size, playerx, playery, player speed
+#player size, player x and y, player speed
 Psize = 20
 Px, Py = 300 - (Psize / 2), 200 - (Psize / 2)
 speed = 5
 
+#falling/jumping variables
 verticalchange = 0
 
 fallspeed = 0.5
@@ -19,26 +20,27 @@ jumpspeed = 3
 maxjumpspeed = jumpspeed
 minjumpspeed = fallspeed
 
-#bitmap stored in 2D array
-grid = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,1,1,1,1,0,0,0,0,0,0,0,0,0],
-        [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,1,0,0,1,0,0,0,1,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+#tilemap stored in 2D array
+grid = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,0,0,0],
+        [1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0],
+        [1,0,0,1,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
 
-#converting bitmap into an array of rect objects
+#converting tilemap into an array of rect objects
+blocksize = Psize * 2
 blocks = []
 for y, row in enumerate(grid):
     for x, space in enumerate(row):
         if row[x] == 1:
-            blocks.append(pygame.Rect(x * 40, y * 40, 40, 40))
+            blocks.append(pygame.Rect(x * blocksize, y * blocksize, blocksize, blocksize))
 
-#game loop
+#gameloop
 run = True
 while run:
     for event in pygame.event.get():
@@ -47,44 +49,50 @@ while run:
             
     #movement
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and Px - speed >= 0:
+    #move left
+    if keys[pygame.K_LEFT]:
         move = True
         for block in blocks:
             if pygame.Rect(Px - speed, Py, Psize, Psize).colliderect(block):
                 move = False
         if move == True:
-            Px -= speed
-            
-    if keys[pygame.K_RIGHT] and Px + speed <= scrn.get_width() - Psize:
+            for x, block in enumerate(blocks):
+                blocks.pop(x)
+                blocks.insert(x, pygame.Rect.move(block, speed, 0))
+    
+    #move right
+    if keys[pygame.K_RIGHT]:
         move = True
         for block in blocks:
             if pygame.Rect(Px + speed, Py, Psize, Psize).colliderect(block):
                 move = False
         if move == True:
-            Px += speed
-            
-    #jumping and falling
+            for x, block in enumerate(blocks):
+                blocks.pop(x)
+                blocks.insert(x, pygame.Rect.move(block, -speed, 0))
+                
+    #falling and jumping
     #falling
     falling = True
     for block in blocks:
         if pygame.Rect(Px, Py + verticalchange + 1, Psize, Psize).colliderect(block):
             falling = False
             verticalchange = 0
-
+            
     if falling == True and fallspeed < maxfallspeed:
         verticalchange += fallspeed
-        
+    
     #jumping
     if keys[pygame.K_UP] and falling == False:
         jumping = True
-
+        
     if jumping == True:
         verticalchange -= jumpspeed
         jumpspeed -= fallspeed
         if jumpspeed <= minjumpspeed:
             jumpspeed = maxjumpspeed
             jumping = False
-
+    
     #moving the player vertically and managing vertical collision
     move = True
     for block in blocks:
@@ -93,15 +101,22 @@ while run:
             move = False
     if move == True:
         Py += verticalchange
-    
-    #updating the player rect object each frame so that if Px or Py change it will me modified accordingly
+        #swap the line above with the 3 lines of code below this to add vertical camera movement
+        #for x, block in enumerate(blocks):
+            #blocks.pop(x)
+            #blocks.insert(x, pygame.Rect.move(block, 0, -verticalchange))
+        
+    #updating the player each frame
     player = pygame.Rect(Px, Py, Psize, Psize)
     
-    #displaying all
+    #displaying the game
     scrn.fill("black")
-    pygame.draw.rect(scrn, "green", player)
+    pygame.draw.rect(scrn, "yellow", player)
     for block in blocks:
-        pygame.draw.rect(scrn, "red", block)
+        pygame.draw.rect(scrn, "blue", block)
     pygame.display.flip()
+    
+    #capping the frame rate at 60
     clock.tick(60)
+    
 pygame.quit()
